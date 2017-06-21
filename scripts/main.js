957a2b87639e5a45c56a55a26a46ab84cec1e8a2
pochoglopez <pochoglopez@gmail.com>
2017-06-20 23:35:41 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+var miTitulo = document.querySelector('h1');
+miTitulo.innerHTML = 'Hello world!';
+
+var miImage = document.querySelector('img');
+
+miImage.onclick = function() {
+    var miSrc = miImage.getAttribute('src');
+    if(miSrc === 'images/firefox-icon.png') {
+      miImage.setAttribute ('src','images/attack.png');
+    } else {
+      miImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
+//-------------------------------------------------
+var miBoton = document.querySelector('button');
+var miTitulo = document.querySelector('h1');
+//-----------------------------------------------------
+
+function seleccionnombre() {
+    var miNombre = prompt('por favor ingresa tu nombre.');
+    localStorage.setItem('nombre', miNombre);
+    miTitulo.innerHTML = 'Mozilla is cool, ' + miNombre;
+}
+
+if(!localStorage.getItem('nombre')) {
+    seleccionnombre();
+}
+else {
+    var storedName = localStorage.getItem('nombre');
+    miTitulo.innerHTML = 'Mozilla is cool, ' + storedName;
+}  
+
+miBoton.onclick = function() {
+    seleccionnombre();
+}
